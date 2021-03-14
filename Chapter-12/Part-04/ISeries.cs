535bd59174d663b73f8082ccc17e329edcf46c2b
Vladimir1965 <zakharenkov1965@yandex.ru>
2021-03-14 23:15:30 +0300

    Chapter-12 Part-04

---
+++ Chapter-12/Part-04/ISeries.cs
@@ -0,0 +1,9 @@
+﻿namespace Part_04
+{
+    interface ISeries
+    {
+        int GetNext(); // возвратить следующее по порядку число
+        void Reset(); // перезапустить
+        void SetStart(int х);
+    }
+}
