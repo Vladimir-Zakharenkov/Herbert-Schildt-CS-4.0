71db98942d661ca0ca5ce769bf41e79bd4d8949a
Vladimir1965 <zakharenkov1965@yandex.ru>
2021-04-14 10:23:46 +0300

    Chapter-12 Part-04

--- Chapter-12/Part-04/ISeries.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Part_04
-{
-    interface ISeries
-    {
-        int GetNext(); // возвратить следующее по порядку число
-        void Reset(); // перезапустить
-        void SetStart(int х);
-    }
-}
--- Chapter-12/Part-04/Program.cs
+++ Chapter-12/Part-04/Program.cs
@@ -1,10 +1,20 @@
-﻿// В классах, реализующих интерфейсы, разрешается и часто практикуется определять
+﻿#region Russian
+
+// В классах, реализующих интерфейсы, разрешается и часто практикуется определять
 // их собственные дополнительные члены. В качестве примера ниже приведен другой
 // вариант класса ByTwos, в который добавлен метод GetPrevious(), возвращающий
 // предыдущее значение.
 
 //Реализовать интерфейс ISeries и добавить в класс ByTwos метод GetPrevious().
-using Part_04;
+using System;
+
+public interface ISeries
+{
+    int GetNext(); // возвратить следующее по порядку число
+    void Reset(); // перезапустить
+    void SetStart(int х); // задать начальное значение
+}
+
 
 class ByTwos : ISeries
 {
@@ -51,3 +61,59 @@ class ByTwos : ISeries
 // интерфейс для этих методов остается прежним, то такие изменения не вызывают
 // никаких осложнений и не нарушают уже существующий код. В этом и заключается
 // одно из преимуществ интерфейсов.
+
+#endregion
+
+#region English
+
+// It is both permissible and common for classes that implement interfaces to define
+// additional members of their own. For example, the following version of ByTwos adds
+// the method GetPrevious(), which returns the previous value:
+
+// Implement ISeries and add GetPrevious(). 
+// class ByTwos : ISeries
+// {
+//    int start;
+//    int val;
+//    int prev;
+
+//    public ByTwos()
+//    {
+//        start = 0;
+//        val = 0;
+//        prev = -2;
+//    }
+
+//    public int GetNext()
+//    {
+//        prev = val;
+//        val += 2;
+//        return val;
+//    }
+
+//    public void Reset()
+//    {
+//        val = start;
+//        prev = start - 2;
+//    }
+
+//    public void SetStart(int x)
+//    {
+//        start = x;
+//        val = start;
+//        prev = val - 2;
+//    }
+
+//    // A method not specified by ISeries. 
+//    public int GetPrevious()
+//    {
+//        return prev;
+//    }
+// }
+
+// Notice that the addition of GetPrevious( ) required a change to the implementations of the
+// methods defined by ISeries. However, since the interface to those methods stays the same,
+// the change is seamless and does not break preexisting code. This is one of the advantages
+// of interfaces.
+
+#endregion
